bff9397d3c4ba25a5cc5f8e2eb99db9a48ca4d4b
GunDumMan <55555topkung@gmail.com>
2022-06-15 14:12:38 +0700

    detail page

--- script.js
+++ script.js
@@ -51,6 +51,12 @@ function addtable(movie) {
     poom2.classList.add('mt-2')
     poom2.setAttribute('id', 'favButton')
 
+    poom.addEventListener('click', function () {
+        movieResults.innerHTML = ''
+        detailCard.innerHTML = ''
+        showDetailBlock(movie)
+    })
+
     poom2.addEventListener('click', function () {
         poom2.style.backgroundColor = 'salmon';
         poom2.style.color = 'white';
@@ -61,9 +67,6 @@ function addtable(movie) {
     const icon = document.createElement('i')
     icon.classList.add('bi')
     icon.classList.add('bi-heart-fill')
-
-
-
     icon.classList.add('me-1')
 
     poom2.appendChild(icon)
@@ -116,13 +119,13 @@ function addtablePromos(movie) {
     para.classList.add('card-text')
     para.classList.add('text-center')
     para.classList.add('text-light')
-    para.innerHTML = movie.title
+    para.innerHTML = movie.entry.title + " " + movie.title
 
     const poom = document.createElement('button')
     poom.setAttribute('type', 'button')
 
     poom.classList.add('btn')
-    poom.classList.add('btn-outline-warning')
+    poom.classList.add('btn-outline-success')
     poom.classList.add('mx-3')
     poom.innerHTML = 'Watch'
 
@@ -131,12 +134,147 @@ function addtablePromos(movie) {
     })
 
     body.appendChild(para)
+
+    body.appendChild(para)
     bot.appendChild(body)
     bot.appendChild(poom)
     card.appendChild(bot)
     item.appendChild(card)
 
     movieResults.appendChild(item)
+
+}
+
+function showDetailBlock(movie) {
+    const detailCard = document.getElementById('detailCard')
+    let row = document.createElement('div')
+    row.classList.add('row')
+
+    let col1 = document.createElement('div')
+    col1.classList.add('col')
+    col1.innerHTML = '1'
+
+    row.appendChild(col1)
+
+    let col2 = document.createElement('div')
+    col2.classList.add('col-9')
+    col2.classList.add('bg-dark')
+
+    let card = document.createElement('div')
+    card.classList.add('card')
+    card.classList.add('mb-3')
+    card.classList.add('bg-darkSecondary')
+
+    let rowCard = document.createElement('div')
+    rowCard.classList.add('row')
+    rowCard.classList.add('g-0')
+
+    let colImg = document.createElement('div')
+    colImg.classList.add('col-md-4')
+
+    let img = document.createElement('img')
+    img.setAttribute('src', movie.images.jpg.large_image_url)
+    img.classList.add('img-fluid')
+    img.classList.add('rounded-start')
+    img.classList.add('img2')
+
+    colImg.appendChild(img)
+    rowCard.appendChild(colImg)
+
+    let colBody = document.createElement('div')
+    colBody.classList.add('col-md-8')
+
+    let innerBody = document.createElement('div')
+    innerBody.classList.add('card-body')
+
+    let ratingButton = document.createElement('button')
+    ratingButton.setAttribute('type', 'button')
+    ratingButton.classList.add('btn')
+    ratingButton.classList.add('btn-outline-secondary')
+    ratingButton.classList.add('mb-2')
+    ratingButton.classList.add('disabled')
+
+    ratingButton.innerHTML = movie.rating
+
+    let nameEng = document.createElement('h2')
+    nameEng.classList.add('card-title')
+    nameEng.classList.add('text-light')
+    nameEng.classList.add('fs-2')
+    nameEng.innerHTML = movie.title
+
+    let nameJapan = document.createElement('h2')
+    nameJapan.classList.add('card-title')
+    nameJapan.classList.add('text-light')
+    nameJapan.classList.add('fs-6')
+    nameJapan.innerHTML = movie.title_japanese
+
+    let synopsis = document.createElement('h3')
+    synopsis.classList.add('text-light')
+    synopsis.classList.add('fs-4')
+    synopsis.classList.add('pt-4')
+    synopsis.classList.add('border-bottom')
+    synopsis.classList.add('pb-2')
+    synopsis.innerHTML = 'synopsis'
+
+    let detailSynopsis = document.createElement('p')
+    detailSynopsis.classList.add('card-text')
+    detailSynopsis.classList.add('text-light')
+    detailSynopsis.innerHTML = movie.synopsis
+
+    const poom2 = document.createElement('button')
+    poom2.setAttribute('type', 'button')
+
+    poom2.classList.add('btn')
+    poom2.classList.add('btn-outline-danger')
+    poom2.classList.add('mx-3')
+    poom2.classList.add('mt-2')
+    poom2.setAttribute('id', 'favButton')
+    const icon = document.createElement('i')
+    icon.classList.add('bi')
+    icon.classList.add('bi-heart-fill')
+    icon.classList.add('me-1')
+
+    poom2.appendChild(icon)
+
+    poom2.innerHTML += "Favorite"
+
+    poom2.addEventListener('click', function () {
+        poom2.style.backgroundColor = 'salmon';
+        poom2.style.color = 'white';
+    })
+
+
+
+
+    innerBody.appendChild(ratingButton)
+    innerBody.appendChild(nameEng)
+    innerBody.appendChild(nameJapan)
+    innerBody.appendChild(synopsis)
+    innerBody.appendChild(detailSynopsis)
+
+
+
+    let col3 = document.createElement('div')
+    col3.classList.add('col')
+    col3.innerHTML = '3'
+
+    colBody.appendChild(innerBody)
+    colBody.appendChild(poom2)
+    rowCard.appendChild(colBody)
+    card.appendChild(rowCard)
+    col2.appendChild(card)
+    row.appendChild(col2)
+    row.appendChild(col3)
+    detailCard.appendChild(row)
+
+
+
+
+
+
+
+
+
 
 }
 
